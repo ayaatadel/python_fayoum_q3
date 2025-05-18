aa45a5587941fd996fae66206179867e0b18e501
ayaatadel <ayaatadel128@gmail.com>
2025-05-18 11:58:56 +0300

    style:adding srtyle

--- Day2/script.js
+++ Day2/script.js
@@ -1,77 +1,59 @@
-document.writeln("mmmmmmmmmmmmmmm") 
+document.writeln("mmmmmmmmmmmmmmm");
 console.log("sdahdh");
 
-document.writeln("hello")
-console.log("hello");
-console.log("hello");
-console.log("hello");
-console.log("hello");
+document.writeln("hello");
+console.log("test");
+console.log("test");
+console.log("test");
+console.log("test");
 
-document.writeln("hello")
-document.writeln("hello")
-document.writeln("hello")
-document.writeln("nnnnnnnnnn")
-document.writeln("nnnnnnnnnn")
-document.writeln("nnnnnnnnnn")
-document.writeln("nnnnnnnnnn")
-document.writeln("nnnnnnnnnn")
-document.writeln("nnnnnnnnnn")
-document.writeln("nnnnnnnnnn")
+document.writeln("test");
+document.writeln("test");
+document.writeln("test");
+document.writeln("nnnnnnnnnn");
+
 console.log("Day2");
 
-document.getElementsByTagName("p")[0]
+document.getElementsByTagName("p")[0];
 console.log("document");
 
-function sum(x,y)
-{
-    console.log(z);
-    console.log(x+y);
-    console.log(w);
-    console.log(w);
-    console.log(w);
-    console.log(w);
-    console.log(w);
-    
+function sum(x, y) {
+  console.log(z);
+  console.log(x + y);
+  console.log(w);
+  console.log(w);
+  console.log(w);
+  console.log(w);
+  console.log(w);
 }
 
-function test()
-{
-    sum()
-    x()
-    y()
+function test() {
+  sum();
+  x();
+  y();
 }
 function test() {
-    console.log("test");
-    
+  console.log("test");
 }
-function x(x,y)
-{
-    console.log(w);
-    
-}function hello(x,y)
-{
-    console.log(x+y);
-    
-}function test(xx)
-{
-    console.log(x+y);
- 1}   
+function x(x, y) {
+  console.log(w);
+}
+function test(x, y) {
+  console.log(x + y);
+}
+function test(xx) {
+  console.log(x + y);
+  1;
+}
 console.log("hello");
-function sum(x,y)
-{
-    console.log(x+y);
-    
+function sum(x, y) {
+  console.log(x + y);
 }
-document.writeln("sndaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-function y(x,y)
-{
-    console.log(x+y);
-    
+document.writeln("sndaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+function y(x, y) {
+  console.log(x + y);
 }
-
-
 
 function name(params) {
-    console.log("name");
-    
+  console.log("name");
 }
